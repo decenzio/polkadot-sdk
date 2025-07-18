c7c1ca498ec8a77b6efb72bdfce2a18fd705ff57
Branislav Hozza <brankohozza@gmail.com>
2025-07-18 12:32:08 +0200

    fix: maybe?

--- templates/parachain/runtime/src/configs/mod.rs
+++ templates/parachain/runtime/src/configs/mod.rs
@@ -30,7 +30,7 @@ use polkadot_sdk::{staging_parachain_info as parachain_info, staging_xcm as xcm,
 use polkadot_sdk::{staging_xcm_builder as xcm_builder, staging_xcm_executor as xcm_executor};
 
 // Substrate and Polkadot dependencies
-use cumulus_pallet_parachain_system::RelayNumberMonotonicallyIncreases;
+use cumulus_pallet_parachain_system::{RelayNumberMonotonicallyIncreases, RelaychainDataProvider};
 use cumulus_primitives_core::{AggregateMessageOrigin, ParaId};
 use frame_support::{
 	derive_impl,
@@ -369,7 +369,7 @@ impl pallet_nfts::Config for Runtime {
 	#[cfg(feature = "runtime-benchmarks")]
 	type Helper = ();
 	type WeightInfo = ();
-	type BlockNumberProvider = ();
+	type BlockNumberProvider = RelaychainDataProvider<Runtime>;
 }
 
 impl pallet_utility::Config for Runtime {
