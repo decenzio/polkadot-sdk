ed5366a4deacb971c267b35fa90696c02712388c
Branislav Hozza <brankohozza@gmail.com>
2025-10-09 23:17:08 +0200

    fix: ?

--- templates/parachain/runtime/src/configs/mod.rs
+++ templates/parachain/runtime/src/configs/mod.rs
@@ -434,7 +434,7 @@ impl pallet_staking::Config for Runtime {
 	type BondingDuration = BondingDuration;
 	type SlashDeferDuration = SlashDeferDuration;
 	type AdminOrigin = EnsureRoot<AccountId>;
-	type SessionInterface = Session;
+	type SessionInterface = ();
 	type EraPayout = pallet_staking::ConvertCurve<RewardCurve>;
 	type NextNewSession = Session;
 	type MaxExposurePageSize = ConstU32<256>;
